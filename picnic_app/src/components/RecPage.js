13ef9e0baba0802e18f5dfd605967c87198d5780
ziv talmon <zivtalmon89@gmail.com>
2022-12-31 21:53:55 +0200

    useless import removed

--- picnic_app/src/components/RecPage.js
+++ picnic_app/src/components/RecPage.js
@@ -1,7 +1,6 @@
 import { useState,useEffect } from "react"
 import { useParams } from "react-router-dom"
 import RecMap from "./RecMap";
-import ErrorBoundary from "./ErrorBoundary";
 
 const RecPage = () => {
     const {id} = useParams();
