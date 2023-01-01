ef7c5344d7aa0aae922bde18b9f6df66dd5f6452
ziv talmon <zivtalmon89@gmail.com>
2023-01-01 20:07:46 +0200

    changed directory

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const {db} = require('./config/db.js')
+const path = require('path')
 
 dotenv.config()
 const app = express()
@@ -15,12 +16,12 @@ app.listen(process.env.PORT||5000, () => {
     console.log(`run on port ${process.env.PORT}||5000`);
 })
 
-const __dirname = path.resolve(); 
+console.log(__dirname);
  
 app.use(express.static(path.join(__dirname, './picnic_app/build'))) 
  
 app.get('*', (req,res)=>{ 
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html')) 
+  res.sendFile(path.resolve(__dirname, './picnic_app/build', 'index.html')) 
 })
 
 app.get('/provisions', (req,res) => {
