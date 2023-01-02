ca352136ce71cbd20022716dec7da3254bbb1d3f
ziv talmon <zivtalmon89@gmail.com>
2023-01-02 20:26:45 +0200

    server changed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}))
 
 console.log(__dirname);
  
-app.use(express.static(path.join(__dirname, './picnic_app/build'))) 
+app.use(express.static(path.join(__dirname, 'build'))) 
  
 app.get('', (req,res)=>{ 
   res.sendFile(path.join(__dirname, 'build','index.html')) 
