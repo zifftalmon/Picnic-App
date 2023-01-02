1895fd5f14b84e505b76d2587caf4af41b92efc4
ziv talmon <zivtalmon89@gmail.com>
2023-01-02 23:30:28 +0200

    draft 2.1

--- picnic_app/src/components/Provisions.js
+++ picnic_app/src/components/Provisions.js
@@ -6,7 +6,7 @@ const Provisions = () => {
 
 
     useEffect(() => {
-        fetch('/provisions')
+        fetch('http://localhost:3001//provisions')
             .then(res => res.json())
             .then(data => data.map(item => { return setList(list => [...list, item.name]) }))
     }, [])
@@ -14,7 +14,7 @@ const Provisions = () => {
 
 
     const handleSubmit = (e) => {
-        fetch('/provisions', {
+        fetch('http://localhost:3001//provisions', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
